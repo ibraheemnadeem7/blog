9c1c35f6feba4bcdea4e48f1faee225a2d2e94c4
ibraheemnadeem7 <157302504+ibraheemnadeem7@users.noreply.github.com>
2024-04-10 20:50:13 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "/articles", to: "articles#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
